40d8d7a801bc3fad93662e229c2703b07b06304a
Von Kaukeano <von.kaukeano@temple.edu>
2019-10-15 20:55:03 -0400

    Fix

--- encoder_alg.py
+++ encoder_alg.py
@@ -1,35 +1,55 @@
 import RPi.GPIO as GPIO
 import time
-import threading
 
-GPIO.setmode(GPIO.BOARD)
+sensor = 18  # define the GPIO pin our sensor is attached to
 
-ENCODER = 4
-lasttime = 0
+GPIO.setmode(GPIO.BCM)  # set GPIO numbering system to BCM
+GPIO.setup(sensor, GPIO.IN)  # set our sensor pin to an input
 
+sample = 2  # Number of tick counts between calculations
+N_Windows = 1  # N windows per rotation
+count = 0
+
+start = 0
+end = 0
+
+
+def set_start():
+    global start
+    start = time.time()
+
+
+def set_end():
+    global end
+    end = time.time()
+
+
+def get_rpm(c):
+    global count  # delcear the count variable global so we can edit it
+
+    if not count:
+        set_start()  # create start time
+        count = count + 1  # increase counter by 1
+    else:
+        count = count + 1
+
+    if count == sample:
+        set_end()  # create end time
+        delta = end - start  # time taken to do a half rotation in seconds
+        # delta = delta / 60  # converted to minutes
+        # converted to time for a full single rotation
+        # rpm = (sample / delta) / (960) #1:120 is the gear ratio
+        print delta
+        time.sleep(.001)
+        count = 0  # reset the count to 0
+
+
+# execute the get_rpm function when a HIGH signal is detected
+GPIO.add_event_detect(sensor, GPIO.RISING, callback=get_rpm)
 
 try:
-    def callback_up(channel):
-        print(time.ctime())
-        threading.Timer(.004, callback_up).start()
-        global lasttime
-        if lasttime == 0:
-            lasttime = time.time()
-        else:
-            now = time.time()
-            gap = now-lasttime
-            print(gap)
-            lasttime = now
-
-        GPIO.add_event_detect(ENCODER, GPIO.RISING, callback=callback_up)
-        while 1:
-            time.sleep(10)
-
-        callback_up()
-
+    while True:  # create an infinte loop to keep the script running
+        time.sleep(0.1)
 except KeyboardInterrupt:
-    GPIO.cleanup()
-
-
-finally:
+    print "  Quit"
     GPIO.cleanup()
